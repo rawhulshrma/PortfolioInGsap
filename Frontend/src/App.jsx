2addacd5eb9dfb4a0984fbaa7615cfeb1156b167
Rahul Versatile <rahul.sharma@versatileitsolution.com>
2024-06-05 19:31:09 +0530

    Updated project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Main from "./Pages/Main";
 import Loader from './Pages/Loader/Loader'
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, loadUser } from "./actions/user";
 
 function App() {
-  const[isLoading, setIsLoading] = useState(true);
-  useEffect(()=>{
-    setTimeout(()=>{
-      setIsLoading(false);
-    }, 4500)
-  },[])
+  const dispatch = useDispatch();
+
+  const { isAuthenticated } = useSelector((state) => state.login);
+  const { loading, user } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(getUser());
+    dispatch(loadUser());
+  }, [dispatch]);
 
   return (
-    isLoading?
-    <Loader/>
-    :
-    <div className="App">
-      <Main/>
-    </div>
+    loading ? (
+      <Loader />
+    ) : (
+      <div className="App">
+        <Main/>
+      </div>
+    )
   );
 }
 
